8fb4a3129de6bc397fd63209512b60223cacd6cc
Bill Lawson <blawson@firstinspires.org>
2020-03-05 09:00:45 -0500

    add console client to access Api

---
+++ src/Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+
+namespace Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //discover endpoints from discovery document metadata
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            //request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+        }
+    }
+}
